2516d9e0f755c0eb93bb82800a25ce9aff8b0de8
Prasannalakshmi G <prasannal2kshmi@gmail.com>
2026-07-07 10:52:46 +0530

    Committing 1 item from workspace aef51ebd-2b5c-4dc3-a539-fcdfe65a1028

---
+++ InitialVersion/WH_Demo_CICD.Warehouse/dbo/Views/vw_EmployeeReport.sql
@@ -0,0 +1,7 @@
+-- Auto Generated (Do not modify) AF0F8D6E7CE3D584DDB1BD93E6A47E4375916E9E28C926272ADEDEA104A4A627
+CREATE VIEW dbo.vw_EmployeeReport
+AS
+SELECT
+    EmployeeName,
+    Salary
+FROM dbo.EmployeeSalary;
